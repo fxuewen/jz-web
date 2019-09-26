6a6b4debe76593973fec5569e0f7fabed0629a65
fuxuewen <1012155589@qq.com>
2019-09-26 10:19:40 +0800

    首页代码提交

---
+++ components/js/custom-card-show.js
@@ -0,0 +1,50 @@
+Vue.component('custom-card-show', {
+  render: function(createElement) {
+    return createElement('div', { class: 'custom-card-container' }, [
+      createElement('div', { class: 'card-header clearfix' }, [
+        createElement('span', { class: 'title', domProps: { innerHTML: this.item.title } }),
+        createElement('div', { class: 'card-header-btn' }, [
+          createElement('div', { class: 'card-header-btn-edit' }, [
+            createElement('i', { class: 'el-icon-edit' }),
+            createElement('span', { domProps: { innerHTML: '编辑' } })
+          ]),
+          createElement('div', { class: 'card-header-btn-delete' }, [
+            createElement('i', { class: 'el-icon-delete' }),
+            createElement('span', { domProps: { innerHTML: '删除' }, on: { click: this.deleteCard } })
+          ])
+        ])
+      ]),
+      createElement('div', { class: 'card-content' }, [createElement('img', { attrs: { src: this.item.imgUrl } })])
+    ]);
+  },
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    // 删除卡片
+    deleteCard() {
+      this.$confirm('是否删除?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$emit('card-delete', this.item);
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+    }
+  }
+});
--- js/default.js
+++ js/default.js
@@ -1,8 +1,4 @@
 $(window).load(function() {
-  layui.use('element', function() {
-    var element = layui.element;
-  });
-
   $('.custom-router-link').bind('click', function(e) {
     e.preventDefault();
     var path = $(this).attr('route-path') || '';
