e5234e50afa5726aac755bd1c1af10c75f0b58c8
fuxuewen <1012155589@qq.com>
2019-09-26 14:43:10 +0800

    导航样式调整

--- js/default.js
+++ js/default.js
@@ -1,4 +1,19 @@
 $(window).load(function() {
+  var path = window.location.pathname;
+  var links = $('.custom-router-link');
+  links.each(function() {
+    if ($(this).attr('route-path') === path) {
+      var parentNode = this.parentNode;
+
+      if ($(parentNode).hasClass('layui-nav-item')) {
+        $(parentNode).addClass('layui-this');
+      } else {
+        $(parentNode).addClass('layui-this');
+        $(parentNode.parentNode.parentNode).addClass('layui-nav-itemed');
+      }
+    }
+  });
+  console.log('location', window.location);
   $('.custom-router-link').bind('click', function(e) {
     e.preventDefault();
     var path = $(this).attr('route-path') || '';
